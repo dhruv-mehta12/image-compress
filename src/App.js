c8a7cd46316de4add1e8e3e9907a6604dd034432
dhruv-mehta12 <mehtadhruv2k1@gmail.com>
2023-08-17 22:21:37 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import FileUploadForm from './form';
+
+function App() {
+  return (
+    <div className="App">
+      <FileUploadForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/form.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import * as imageConversion from 'image-conversion';
+import { url } from "./backend"
+
+const FileUploadForm = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [compressed, setCompressed] = useState(null); 
+  const [status, setStatus] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  const handleFileUpload = async () => {
+    if (selectedFile) {
+      const res = await imageConversion.compressAccurately(selectedFile, {
+        size: 65,    
+        accuracy: 0.9,
+        type: "image/jpeg",
+        scale: 0.3,
+      })
+      setCompressed(res)
+      var fd = new FormData();
+      fd.append('file', res, selectedFile.name);
+
+      const response = await fetch(`${url}/upload`, {method:"POST", body: fd})
+      setStatus(response.status)
+    } else {
+      alert('Please select a file before uploading.');
+    }
+  };
+
+  return (
+    <div>
+      <h1>File Upload Form</h1>
+      <input type="file" name='file' onChange={handleFileChange} />
+      <button onClick={handleFileUpload}>compress</button>
+      {
+        status === 200 ? <div><p>original: {selectedFile.size}B</p><br /><p>compressed: {compressed.size}B</p><br/><a href={`${url}/get/${selectedFile.name}-compressed.jpg`}>download</a></div> : null
+      }
+    </div>
+  );
+};
+
+export default FileUploadForm;
